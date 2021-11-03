653887d3872ea1d60de01bd8c1bea577c4d08a62
secretshardul <shardul.aeer@somaiya.edu>
2021-11-03 11:54:00 +0530

    feat: Initialize contract instruction

--- programs/cyclos-protocol-v2/src/lib.rs
+++ programs/cyclos-protocol-v2/src/lib.rs
@@ -1,14 +1,39 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod cyclos_protocol_v2 {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, bump: u8) -> ProgramResult {
+        ctx.accounts.factory_state.bump = bump;
+        ctx.accounts.factory_state.owner = ctx.accounts.owner.key();
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(bump: u8)]
+pub struct Initialize<'info> {
+
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [],
+        bump = bump,
+        payer = owner,
+        space = size_of::<FactoryState>()
+    )]
+    pub factory_state: Account<'info, FactoryState>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct FactoryState {
+    pub bump: u8,
+    pub owner: Pubkey,
+}
--- tests/cyclos-protocol-v2.ts
+++ tests/cyclos-protocol-v2.ts
@@ -1,17 +1,26 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { CyclosProtocolV2 } from '../target/types/cyclos_protocol_v2';
+const { PublicKey } = anchor.web3;
 
-describe('cyclos-protocol-v2', () => {
+describe('cyclos-protocol-v2', async () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.CyclosProtocolV2 as Program<CyclosProtocolV2>;
 
+  const [factoryState, factoryStateBump] = await PublicKey.findProgramAddress([], program.programId);
+
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const tx = await program.rpc.initialize(factoryStateBump, {
+      accounts: {
+        owner: anchor.getProvider().wallet.publicKey,
+        factoryState,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    });
     console.log("Your transaction signature", tx);
   });
 });
