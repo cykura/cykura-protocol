c78a25edb986729bbe0f36f50cb35451910d1850
Aadhinana <ajibaa@gmail.com>
2021-12-09 11:06:24 +0530

    test: nft - liquidity_amounts

--- programs/non-fungible-position-manager/src/libraries/liquidity_amounts.rs
+++ programs/non-fungible-position-manager/src/libraries/liquidity_amounts.rs
@@ -2,7 +2,6 @@
 ///! Provides functions for computing liquidity amounts from token amounts and prices
 ///! Implements formula 6.29 and 6.30
 ///
-
 use cyclos_core::libraries::fixed_point_x32;
 use muldiv::MulDiv;
 use std::convert::TryFrom;
@@ -26,11 +25,16 @@ pub fn get_liquidity_for_amount_0(
         std::mem::swap(&mut sqrt_ratio_a_x32, &mut sqrt_ratio_b_x32);
     };
 
-    let intermediate = sqrt_ratio_a_x32.mul_div_floor(sqrt_ratio_b_x32, fixed_point_x32::Q32).unwrap();
+    let intermediate = sqrt_ratio_a_x32
+        .mul_div_floor(sqrt_ratio_b_x32, fixed_point_x32::Q32)
+        .unwrap();
 
     u32::try_from(
-        amount_0.mul_div_floor(intermediate, sqrt_ratio_b_x32 - sqrt_ratio_a_x32).unwrap()
-    ).unwrap()
+        amount_0
+            .mul_div_floor(intermediate, sqrt_ratio_b_x32 - sqrt_ratio_a_x32)
+            .unwrap(),
+    )
+    .unwrap()
 }
 
 /// Computes the amount of liquidity received for a given amount of token_1 and price range
@@ -53,8 +57,11 @@ pub fn get_liquidity_for_amount_1(
     };
 
     u32::try_from(
-        amount_1.mul_div_floor(fixed_point_x32::Q32, sqrt_ratio_b_x32 - sqrt_ratio_a_x32).unwrap()
-    ).unwrap()
+        amount_1
+            .mul_div_floor(fixed_point_x32::Q32, sqrt_ratio_b_x32 - sqrt_ratio_a_x32)
+            .unwrap(),
+    )
+    .unwrap()
 }
 
 /// Computes the maximum amount of liquidity received for a given amount of token0, token1, the current
@@ -88,7 +95,7 @@ pub fn get_liquidity_for_amounts(
         // by token_0 and token_1
         u32::min(
             get_liquidity_for_amount_0(sqrt_ratio_x32, sqrt_ratio_b_x32, amount_0),
-            get_liquidity_for_amount_1(sqrt_ratio_a_x32, sqrt_ratio_x32, amount_1)
+            get_liquidity_for_amount_1(sqrt_ratio_a_x32, sqrt_ratio_x32, amount_1),
         )
     } else {
         // If P ≥ P_upper, only token_1 liquidity is active
@@ -115,10 +122,10 @@ pub fn get_amount_0_for_liquidity(
         std::mem::swap(&mut sqrt_ratio_a_x32, &mut sqrt_ratio_b_x32);
     };
 
-    ((liquidity as u64) << fixed_point_x32::RESOLUTION).mul_div_floor(
-        sqrt_ratio_b_x32 - sqrt_ratio_a_x32,
-        sqrt_ratio_b_x32
-    ).unwrap() / sqrt_ratio_a_x32
+    ((liquidity as u64) << fixed_point_x32::RESOLUTION)
+        .mul_div_floor(sqrt_ratio_b_x32 - sqrt_ratio_a_x32, sqrt_ratio_b_x32)
+        .unwrap()
+        / sqrt_ratio_a_x32
 }
 
 /// Computes the amount of token_1 for a given amount of liquidity and a price range
@@ -140,10 +147,9 @@ pub fn get_amount_1_for_liquidity(
         std::mem::swap(&mut sqrt_ratio_a_x32, &mut sqrt_ratio_b_x32);
     };
 
-    (liquidity as u64).mul_div_floor(
-        sqrt_ratio_b_x32 - sqrt_ratio_a_x32,
-        fixed_point_x32::Q32
-    ).unwrap()
+    (liquidity as u64)
+        .mul_div_floor(sqrt_ratio_b_x32 - sqrt_ratio_a_x32, fixed_point_x32::Q32)
+        .unwrap()
 }
 
 /// Computes the token_0 and token_1 value for a given amount of liquidity, the current
@@ -171,17 +177,171 @@ pub fn get_amounts_for_liquidity(
 
     if sqrt_ratio_x32 <= sqrt_ratio_a_x32 {
         // If P ≤ P_lower, active liquidity is entirely in token_0
-        (get_amount_0_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_b_x32, liquidity), 0)
+        (
+            get_amount_0_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_b_x32, liquidity),
+            0,
+        )
     } else if sqrt_ratio_x32 < sqrt_ratio_b_x32 {
         // If P_lower < P < P_upper, active liquidity is in token_0 and token_1
         (
             get_amount_0_for_liquidity(sqrt_ratio_x32, sqrt_ratio_b_x32, liquidity),
-            get_amount_1_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_x32, liquidity)
+            get_amount_1_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_x32, liquidity),
         )
     } else {
         // If P ≥ P_upper, active liquidity is entirely in token_1
-        (0, get_amount_1_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_b_x32, liquidity))
+        (
+            0,
+            get_amount_1_for_liquidity(sqrt_ratio_a_x32, sqrt_ratio_b_x32, liquidity),
+        )
     }
 }
 
-// TODO tests
+// Need to move this to some common test utils later
+pub fn encode_price_sqrt_x32(reserve_1: u64, reserve_0: u64) -> u64 {
+    ((reserve_1 as f64 / reserve_0 as f64).sqrt() * u64::pow(2, 32) as f64).round() as u64
+}
+
+#[cfg(test)]
+mod liq_amounts {
+    use super::*;
+
+    // get_liquidity_for_amount tests
+    mod liquidity_for_amounts {
+        use super::*;
+
+        #[test]
+        fn price_within_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(1, 1);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity = get_liquidity_for_amounts(
+                price_sqrt_x32,
+                price_sqrt_a_x32,
+                price_sqrt_b_x32,
+                100,
+                200,
+            );
+            assert_eq!(liquidity, 2148);
+        }
+
+        #[test]
+        fn price_below_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(99, 110);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity = get_liquidity_for_amounts(
+                price_sqrt_x32,
+                price_sqrt_a_x32,
+                price_sqrt_b_x32,
+                100,
+                200,
+            );
+            assert_eq!(liquidity, 1048);
+        }
+
+        #[test]
+        fn price_above_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(111, 100);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity = get_liquidity_for_amounts(
+                price_sqrt_x32,
+                price_sqrt_a_x32,
+                price_sqrt_b_x32,
+                100,
+                200,
+            );
+            assert_eq!(liquidity, 2097);
+        }
+
+        #[test]
+        fn price_at_lower_boundary() {
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_x32 = price_sqrt_a_x32;
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity = get_liquidity_for_amounts(
+                price_sqrt_x32,
+                price_sqrt_a_x32,
+                price_sqrt_b_x32,
+                100,
+                200,
+            );
+            assert_eq!(liquidity, 1048);
+        }
+
+        #[test]
+        fn price_at_upper_boundary() {
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let price_sqrt_x32 = price_sqrt_b_x32;
+            let liquidity = get_liquidity_for_amounts(
+                price_sqrt_x32,
+                price_sqrt_a_x32,
+                price_sqrt_b_x32,
+                100,
+                200,
+            );
+            assert_eq!(liquidity, 2097);
+        }
+    }
+
+    // get_amounts_for_liquidity tests
+    mod amounts_for_liquidity {
+        use super::*;
+
+        #[test]
+        fn price_within_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(1, 1);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity =
+                get_amounts_for_liquidity(price_sqrt_x32, price_sqrt_a_x32, price_sqrt_b_x32, 2148);
+            assert_eq!(liquidity.0, 99);
+            assert_eq!(liquidity.1, 99);
+        }
+
+        #[test]
+        fn price_below_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(99, 110);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity =
+                get_amounts_for_liquidity(price_sqrt_x32, price_sqrt_a_x32, price_sqrt_b_x32, 1048);
+            assert_eq!(liquidity.0, 99);
+            assert_eq!(liquidity.1, 0);
+        }
+
+        #[test]
+        fn price_above_range() {
+            let price_sqrt_x32 = encode_price_sqrt_x32(111, 100);
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity =
+                get_amounts_for_liquidity(price_sqrt_x32, price_sqrt_a_x32, price_sqrt_b_x32, 2097);
+            assert_eq!(liquidity.0, 0);
+            assert_eq!(liquidity.1, 199);
+        }
+
+        #[test]
+        fn price_at_lower_boundary() {
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_x32 = price_sqrt_a_x32;
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let liquidity =
+                get_amounts_for_liquidity(price_sqrt_x32, price_sqrt_a_x32, price_sqrt_b_x32, 1048);
+            assert_eq!(liquidity.0, 99);
+            assert_eq!(liquidity.1, 0);
+        }
+
+        #[test]
+        fn price_at_upper_boundary() {
+            let price_sqrt_a_x32 = encode_price_sqrt_x32(100, 110);
+            let price_sqrt_b_x32 = encode_price_sqrt_x32(110, 100);
+            let price_sqrt_x32 = price_sqrt_b_x32;
+            let liquidity =
+                get_amounts_for_liquidity(price_sqrt_x32, price_sqrt_a_x32, price_sqrt_b_x32, 2097);
+            assert_eq!(liquidity.0, 0);
+            assert_eq!(liquidity.1, 199);
+        }
+    }
+}
